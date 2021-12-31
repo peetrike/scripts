daf749a3a4f31f7ea001a2b4cd1e25e659206121
Peter Wawa <meelisn@outlook.com>
2021-12-31 13:36:45 +0200

    Refactor script
    - remove default values
    - make -ScopeId mandatory

--- src/Networking/DHCP/Set-Option119.ps1
+++ src/Networking/DHCP/Set-Option119.ps1
@@ -1,7 +1,7 @@
 ﻿#Requires -Version 3.0
 #Requires -Modules DhcpServer
 
-[cmdletbinding(
+[CmdletBinding(
     SupportsShouldProcess
 )]
 param(
@@ -9,33 +9,12 @@ param(
             ValueFromPipeline
         )]
         [string[]]
-    $DnsSuffix = @(
-        'ipfdigital.net'
-        'ipfdigital.tech'
-        'ipfdigital.io'
-        'bapps.ipfd'
-        'dev01.ipfd'
-        'nonprod.ipfd'
-        'preprod.ipfd'
-        'prod.ipfd'
-        'st01.ipfd'
-        'st02.ipfd'
-        'st03.ipfd'
-        'st04.ipfd'
-        'st05.ipfd'
-        'st06.ipfd'
-        'st07.ipfd'
-        'st08.ipfd'
-        'st09.ipfd'
-        'st10.ipfd'
-        'st11.ipfd'
-        'st12.ipfd'
-        'st13.ipfd'
-        'sit.ipfd'
-        'uat.ipfd'
-    ),
+    $DnsSuffix,
+        [parameter(
+            Mandatory
+        )]
         [ipaddress]
-    $ScopeId = "192.168.0.0"
+    $ScopeId
 )
 
 begin {
