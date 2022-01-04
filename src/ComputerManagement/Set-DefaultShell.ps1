4ac62f2423830b5c5b7eb005378837fe785c584c
Peter Wawa <meelisn@outlook.com>
2022-01-04 08:35:45 +0200

    add script tthat changes default shell
     (Server Core)

---
+++ src/ComputerManagement/Set-DefaultShell.ps1
@@ -0,0 +1,4 @@
+﻿$WinLogonKey = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
+
+    # Set PowerShell as default shell
+Set-ItemProperty -Path $WinLogonKey -Name Shell -Value 'PowerShell.exe -NoExit'
