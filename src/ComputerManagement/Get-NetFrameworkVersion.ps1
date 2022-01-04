9064310d8ec09ae6fe94b68bb0a739ce87620d75
Peter Wawa <meelisn@outlook.com>
2022-01-04 13:04:00 +0200

    Refactor script
    - add .NET Framework 1 detection
    - remove 1 from -Generation default values

--- src/ComputerManagement/Get-NetFrameworkVersion.ps1
+++ src/ComputerManagement/Get-NetFrameworkVersion.ps1
@@ -13,7 +13,7 @@ param (
         [ValidateRange(1, 4)]
         [int[]]
         # Main version of .NET Framework to be included in response
-    $Generation = @(4, 3, 2, 1),
+    $Generation = @(4, 3, 2),
         [ValidateSet('Client', 'Full')]
         [string]
         # .NET 4.0 profile to be reported
@@ -25,38 +25,44 @@ Function Get-NetFrameworkVersion {
     param (
             [ValidateRange(1, 4)]
             [int[]]
-        $Generation = @(4, 3, 2, 1),
+        $Generation = @(4, 3, 2),
             [ValidateSet('Client', 'Full')]
             [string]
         $Type = 'Full'
     )
 
     $dotNetRegistry  = 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP'
-    $dotNet4Registry = 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4'
+    $dotNet4Registry = Join-Path -Path $dotNetRegistry -ChildPath 'v4'
+    $InstalledComponents = 'HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components'
 
     $dotNet4Builds = @{
-        '30319'  = @{ Version = [Version]'4.0'                                                 }
-        '378389' = @{ Version = [Version]'4.5'                                                 }
-        '378675' = @{ Version = [Version]'4.5.1' ; Comment = '(8.1/2012R2)'                    }
-        '378758' = @{ Version = [Version]'4.5.1' ; Comment = '(8/7 SP1/Vista SP2)'             }
-        '379893' = @{ Version = [Version]'4.5.2'                                               }
+        '30319'  = @{ Version = [Version]'4.0' }
+        '378389' = @{ Version = [Version]'4.5' }
+        '378675' = @{ Version = [Version]'4.5.1' ; Comment = '(8.1/2012R2)' }
+        '378758' = @{ Version = [Version]'4.5.1' ; Comment = '(8/7 SP1/Vista SP2)' }
+        '379893' = @{ Version = [Version]'4.5.2' }
         '380042' = @{ Version = [Version]'4.5'   ; Comment = 'and later with KB3168275 rollup' }
-        '393295' = @{ Version = [Version]'4.6'   ; Comment = '(Windows 10)'                    }
-        '393297' = @{ Version = [Version]'4.6'   ; Comment = '(NON Windows 10)'                }
-        '394254' = @{ Version = [Version]'4.6.1' ; Comment = '(Windows 10 1511)'               }
-        '394271' = @{ Version = [Version]'4.6.1' ; Comment = '(NON Windows 10 1511)'           }
-        '394802' = @{ Version = [Version]'4.6.2' ; Comment = '(Windows 10 1607/Srv16)'         }
-        '394806' = @{ Version = [Version]'4.6.2' ; Comment = '(NON Windows 10 1607/Srv16)'     }
-        '460798' = @{ Version = [Version]'4.7'   ; Comment = '(Windows 10 1703)'               }
-        '460805' = @{ Version = [Version]'4.7'   ; Comment = '(NON Windows 10 1703)'           }
-        '461308' = @{ Version = [Version]'4.7.1' ; Comment = '(Windows 10 1709)'               }
-        '461310' = @{ Version = [Version]'4.7.1' ; Comment = '(NON Windows 10 1709)'           }
-        '461808' = @{ Version = [Version]'4.7.2' ; Comment = '(Windows 10 1803)'               }
-        '461814' = @{ Version = [Version]'4.7.2' ; Comment = '(NON Windows 10 1803)'           }
-        '528040' = @{ Version = [Version]'4.8.0' ; Comment = '(Windows 10 1903/1909)'          }
-        '528049' = @{ Version = [Version]'4.8.0' ; Comment = '(Non Windows 10 1903 or newer)'  }
+        '393295' = @{ Version = [Version]'4.6'   ; Comment = '(Windows 10)' }
+        '393297' = @{ Version = [Version]'4.6'   ; Comment = '(NON Windows 10)' }
+        '394254' = @{ Version = [Version]'4.6.1' ; Comment = '(Windows 10 1511)' }
+        '394271' = @{ Version = [Version]'4.6.1' ; Comment = '(NON Windows 10 1511)' }
+        '394802' = @{ Version = [Version]'4.6.2' ; Comment = '(Windows 10 1607/Srv16)' }
+        '394806' = @{ Version = [Version]'4.6.2' ; Comment = '(NON Windows 10 1607/Srv16)' }
+        '460798' = @{ Version = [Version]'4.7'   ; Comment = '(Windows 10 1703)' }
+        '460805' = @{ Version = [Version]'4.7'   ; Comment = '(NON Windows 10 1703)' }
+        '461308' = @{ Version = [Version]'4.7.1' ; Comment = '(Windows 10 1709)' }
+        '461310' = @{ Version = [Version]'4.7.1' ; Comment = '(NON Windows 10 1709)' }
+        '461808' = @{ Version = [Version]'4.7.2' ; Comment = '(Windows 10 1803)' }
+        '461814' = @{ Version = [Version]'4.7.2' ; Comment = '(NON Windows 10 1803)' }
+        '528040' = @{ Version = [Version]'4.8.0' ; Comment = '(Windows 10 1903/1909)' }
+        '528049' = @{ Version = [Version]'4.8.0' ; Comment = '(Non Windows 10 1903 or newer)' }
         #'528209' = @{ Version = [Version]'4.8.0' ; Comment = '(Windows 10 2004)'               }
-        '528372' = @{ Version = [Version]'4.8.0' ; Comment = '(Windows 10 2004)'               }
+        '528372' = @{ Version = [Version]'4.8.0' ; Comment = '(Windows 10 2004 or newer)' }
+        '528449' = @{ Version = [Version]'4.8.0' ; Comment = '(Windows 11 / Server 2022)' }
+    }
+    $dotNet1Builds = @{
+        '{78705f0d-e8db-4b2d-8193-982bdda15ecd}' = 'on supported platforms except for Windows XP Media Center and Tablet PC'
+        '{FDC11A6F-17D1-48f9-9EA3-9051954BAA24}' = 'shipped with Windows XP Media Center 2002/2004 and Tablet PC 2004'
     }
 
     foreach ($g in $Generation) {
@@ -78,22 +84,30 @@ Function Get-NetFrameworkVersion {
                         }
                     }
             }
-            Default {
-                Get-ChildItem -Path $dotNetRegistry |
-                    Where-Object { $_.PSChildName -like "v[$g]*" } |
-                    Get-ItemProperty |
-                    ForEach-Object {
-                        $version = [version] $_.Version
-                        New-Object -TypeName PSObject -Property @{
-                            Version = $version
-                            Build   = $version.Build
-                            SP      = $_.SP
+            1 {
+                $version = 'v1.1.4322'
+                $dotNet1Registry = Join-Path -Path $dotNetRegistry -ChildPath $version
+                if (Test-Path -Path $dotNet1Registry) {
+                    $version = [version] $version
+                    New-Object -TypeName PSObject -Property @{
+                        Version = $version
+                        Build   = $version.Build
+                    }
+                } else {
+                    foreach ($component in $dotNet1Builds.Keys) {
+                        $dotNet1Registry = Join-Path -Path $InstalledComponents -ChildPath $component
+                        if (Test-Path $dotNet1Registry -PathType Container) {
+                            $DotNet1Key = Get-ItemProperty -Path $dotNet1Registry
+                            $version = [version] $DotNet1Key.Version.Replace(',', '.')
+                            New-Object -TypeName PSObject -Property @{
+                                Version = $version
+                                Build   = $version.Build
+                                SP      = 'SP{0}' -f $version.Revision
+                                Comment = $dotNet1Builds.$component
+                            }
                         }
                     }
-            }
-        }
-    }
-
+                }
 <#
     $RegistryPrefix = "Registry::";
 #1.0 Original (on supported platforms except for Windows XP Media Center and Tablet PC)
@@ -138,6 +152,22 @@ Function Get-NetFrameworkVersion {
 		}
 	} Catch {}
  #>
+            }
+            Default {
+                Get-ChildItem -Path $dotNetRegistry |
+                    Where-Object { $_.PSChildName -like "v[$g]*" } |
+                    Get-ItemProperty |
+                    ForEach-Object {
+                        $version = [version] $_.Version
+                        New-Object -TypeName PSObject -Property @{
+                            Version = $version
+                            Build   = $version.Build
+                            SP      = $_.SP
+                        }
+                    }
+            }
+        }
+    }
 }
 
 Get-NETFrameworkVersion @PSBoundParameters
