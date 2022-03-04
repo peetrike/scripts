e7bb033aa1752d4dc9d071901b6cbea9c2e80bd2
Peter Wawa <meelisn@outlook.com>
2022-03-04 18:57:03 +0200

    add new scripts

---
+++ src/ComputerManagement/UserProfile/Install-UserProfile.ps1
@@ -0,0 +1,74 @@
+﻿#Requires -Version 5.1
+#Requires -Modules PackageManagement
+#Requires -RunAsAdministrator
+
+<#PSScriptInfo
+    .VERSION 0.0.1
+    .GUID 46c07847-9579-4a58-bb1e-e5b5ad8f7967
+
+    .AUTHOR Peter Wawa
+    .COMPANYNAME Telia Eesti
+    .COPYRIGHT (c) Telia Eesti 2022.  All rights reserved.
+
+    .TAGS user, profile
+
+    .LICENSEURI https://opensource.org/licenses/MIT
+    .PROJECTURI
+    .ICONURI
+
+    .EXTERNALMODULEDEPENDENCIES
+    .REQUIREDSCRIPTS
+    .EXTERNALSCRIPTDEPENDENCIES
+
+    .RELEASENOTES
+        [0.0.1] - 2022-03-04 - Initial release
+
+    .PRIVATEDATA
+#>
+
+<#
+    .SYNOPSIS
+        Installs UserProfile module, if needed
+
+    .DESCRIPTION
+        Installs UserProfile module and updates PowerShellGet module, if needed
+
+    .EXAMPLE
+        Install-UserProfile.ps1
+
+        This example installs module, if needed
+    .INPUTS
+        This script takes no input
+
+    .OUTPUTS
+        Output (if any)
+#>
+
+[CmdletBinding()]
+#[Alias('')]
+[OutputType([void])]
+
+param ()
+
+if (Get-Module UserProfile -ListAvailable) {
+    Write-Verbose -Message 'Module already installed'
+} else {
+        # add TLS 1.2 support, if needed
+    if (-not ([Net.ServicePointManager]::SecurityProtocol -band [Net.SecurityProtocolType]::Tls12)) {
+        [Net.ServicePointManager]::SecurityProtocol =
+            [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+    }
+
+    try {
+        Get-PackageProvider -Name NuGet -ListAvailable -ErrorAction Stop
+    } catch {
+        Install-PackageProvider -Name NuGet -Scope AllUsers -Force
+    }
+
+    if ((Get-Module -Name PowerShellGet -ListAvailable).Version | Where-Object { $_ -gt '2.2.4' }) {
+        Write-Verbose -Message 'PowershellGet already up to date'
+    } else {
+        Install-Module PowerShellGet -Force
+        Remove-Module PowerShellGet, PackageManagement
+    }
+}
---
+++ src/ComputerManagement/UserProfile/clean-OldProfile.ps1
@@ -0,0 +1,75 @@
+#Requires -Modules UserProfile
+
+<#PSScriptInfo
+    .VERSION 0.0.1
+    .GUID 2b1ce67a-2d66-42bb-bf06-cea1db933ac1
+
+    .AUTHOR Peter Wawa
+    .COMPANYNAME Telia Eesti
+    .COPYRIGHT (c) Telia Eesti 2022.  All rights reserved.
+
+    .TAGS user, profile, cleanup
+
+    .LICENSEURI https://opensource.org/licenses/MIT
+    .PROJECTURI https://github.com/peetrike/scripts
+    .ICONURI
+
+    .EXTERNALMODULEDEPENDENCIES UserProfile
+    .REQUIREDSCRIPTS
+    .EXTERNALSCRIPTDEPENDENCIES
+
+    .RELEASENOTES
+        [0.0.1] - 2022-03-04 - Initial release
+
+    .PRIVATEDATA
+#>
+
+<#
+    .SYNOPSIS
+        Clear old user profiles
+
+    .DESCRIPTION
+        Cleanup old user profiles
+
+    .EXAMPLE
+        Clean-OldProfile.ps1 -Days 40
+
+        Find user profiles that are more than 40 days old and remove them
+    .INPUTS
+        This script has no input
+
+    .OUTPUTS
+        Output (if any)
+
+    .NOTES
+        General notes
+
+    .LINK
+        Win32_UserProfile class: https://docs.microsoft.com/previous-versions/windows/desktop/usm/win32-userprofile
+#>
+
+[cmdletbinding(
+    supportsShouldProcess = $true
+)]
+param (
+        [int]
+        # User profile age in days to be deleted
+    $Days = 30,
+        [switch]
+        # remove also corrupted profiles
+    $RemoveCorrupted,
+        [switch]
+        # remove also temporary profiles
+    $RemoveTemporary
+)
+
+$BeforeDate = (get-date).AddDays(-$days)
+Get-UserProfile -Before $BeforeDate | Remove-UserProfile
+
+if ($RemoveCorrupted.IsPresent) {
+    Get-UserProfile -Status Corrupted -ErrorAction Ignore | Remove-UserProfile
+}
+
+if ($RemoveTemporary.IsPresent) {
+    Get-UserProfile -Status Temporary -Loaded $false -ErrorAction Ignore | Remove-UserProfile
+}
