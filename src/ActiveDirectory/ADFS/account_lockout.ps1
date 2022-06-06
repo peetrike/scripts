2d458925c5ae2262508bfd7784d8e83e5591152d
Peter Wawa <meelisn@outlook.com>
2022-06-06 10:52:45 +0300

    Refactor script

--- src/ActiveDirectory/ADFS/account_lockout.ps1
+++ src/ActiveDirectory/ADFS/account_lockout.ps1
@@ -1,24 +1,26 @@
-$_all_adfs_servers = 'ADFS.sm.local'
+[CmdletBinding()]
+param (
+    [string[]]
+    $Server = $env:COMPUTERNAME
+)
+
 $EventFilter = @{
     ProviderName = 'AD FS Auditing'
     ID           =  411
 }
-foreach ($_server in $_all_adfs_servers) {
-    #$eventdata = Get-EventLog -ComputerName $_server -LogName Security | Where {$_.EventID -eq 411}
-    $eventdata = Get-WinEvent -ComputerName $_server -FilterHashTable $EventFilter -Oldest -MaxEvents 100
-    $lockedout = $eventdata | Where-Object {($_.properties[2].value -like '*The referenced account is currently locked out*')}
-    $lockedout | ForEach-Object {
-        $_target_account = $_.Properties[2].Value
-        $_iplock = $_.Properties[4].Value
-        $u = $_target_account.Split('-')[0]
-        $detail = $_target_account.Split('-')[1]
-        $ObjProperties = @{
-            Server  = $_server
-            Time    = $_.TimeCreated
-            Account = $u
-            IP      = $_iplock
-            Detail  = $detail
-        }
-        New-Object PSObject -Property $ObjProperties
-    } | Out-GridView
+foreach ($_server in $Server) {
+    Get-WinEvent -ComputerName $_server -FilterHashTable $EventFilter -Oldest -MaxEvents 100 |
+        Where-Object { $_.properties[2].value -like '*The referenced account is currently locked out*' } |
+        ForEach-Object {
+            $_target_account = $_.Properties[2].Value
+            $u, $detail, $junk = $_target_account.Split('-')
+            $ObjProperties = @{
+                Server  = $_server
+                Time    = $_.TimeCreated
+                Account = $u
+                IP      = $_.Properties[4].Value
+                Detail  = $detail
+            }
+            New-Object PSObject -Property $ObjProperties
+        } | Out-GridView
 }
