363300aa4ab3709893cc437a7d51ea4abe70a04b
Peter Wawa <meelisn@outlook.com>
2022-10-04 13:58:46 +0300

    Release v1.1.1
    -  Support MFA authentication and delegated access with ExO module.
    - Import only set of command when connecting with ExO.

--- src/Office365/Get-DistributionGroupReport.ps1
+++ src/Office365/Get-DistributionGroupReport.ps1
@@ -1,13 +1,14 @@
 ﻿#Requires -Version 3.0
+# Requires -Modules ExchangeOnlineManagement
 
 <#PSScriptInfo
-    .VERSION 1.0.4
+    .VERSION 1.1.1
 
     .GUID 620aced0-5d45-4168-b454-2e603e1d09ca
 
     .AUTHOR Meelis Nigols
     .COMPANYNAME Telia Eesti AS
-    .COPYRIGHT (c) Telia Eesti AS 2019.  All rights reserved.
+    .COPYRIGHT (c) Telia Eesti AS 2022.  All rights reserved.
 
     .TAGS office365 group report
 
@@ -20,6 +21,8 @@
     .EXTERNALSCRIPTDEPENDENCIES
 
     .RELEASENOTES
+        [1.1.1] - Import only set of command when connecting with ExO.
+        [1.1.0] - refactor script: Support MFA authentication and delegated access with ExO module.
         [1.0.4] - refactor script: use existing connection to Exchange Online, when available.
         [1.0.3] - added missing parameter names, changed -Filter to string
         [1.0.2] - changed Import-PSSession so that it imports only required cmdlets
@@ -32,8 +35,8 @@
     .SYNOPSIS
         Generate distribution group members report.
     .DESCRIPTION
-        This script generates Office 365 Distribution Group members report.
-        Report is saved as one .csv file or separate .csv file for each
+        This script collects Office 365 Distribution Group members.
+        The result is saved as one .csv file or separate .csv file for each
         Distribution Group.
 
         When you haven't already established connection to Exchange Online, the
@@ -42,6 +45,10 @@
         Get-DistributionGroupReport -Credential $MyCredential
 
         This example uses previously obtained credential variable.
+    .EXAMPLE
+        Get-DistributionGroupReport -UserPrincipalName me@consoto.com -DelegatedOrganization adatum.com
+
+        This example uses MFA authentication and connects to delegated organization.
     .EXAMPLE
         Get-DistributionGroupReport -OutputFile Multiple
 
@@ -67,38 +74,65 @@
         https://docs.microsoft.com/powershell/exchange/filter-properties
 #>
 
-[CmdletBinding()]
+[CmdletBinding(
+    DefaultParameterSetName = 'MFA'
+)]
 param (
         [string]
         # The Filter parameter indicates the OPath filter used to filter recipients.
     $Filter,
+        [ValidateSet('Single', 'Multiple')]
+        [string]
+        # Specifies whether to create single report file or separate file for every distribution group.
+    $OutputFile = 'Single',
         [parameter(
-            HelpMessage = 'Enter credential for Office365 tenant'
+            HelpMessage = 'Enter credential for Office365 tenant',
+            Mandatory,
+            ParameterSetName = 'Credential'
         )]
         [System.Management.Automation.Credential()]
         [PSCredential]
         # The Credential parameter specifies the user name and password that's used to connect Office 365.
     $Credential,
-        [ValidateSet('Single', 'Multiple')]
+        [parameter(
+            ParameterSetName = 'MFA'
+        )]
         [string]
-        # Specifies whether to create single report file or separate file for every distribution group.
-    $OutputFile = 'Single'
+        # Specifies User name to use for authentication
+    $UserPrincipalName,
+        [parameter(
+            ParameterSetName = 'MFA'
+        )]
+        [string]
+        # Delegated organization domain name to connect
+    $DelegatedOrganization
 )
 
+$CmdletList = 'Get-DistributionGroup', 'Get-DistributionGroupMember'
 try {
     $null = Get-Command Get-DistributionGroup -ErrorAction Stop
     Write-Verbose -Message 'Connection already established'
 } catch {
-    if (-not $Credential) {
-        $Credential = Get-Credential -Message 'Enter credential for Office365 tenant'
-    }
-
     if (Get-Module ExchangeOnlineManagement -ListAvailable) {
-        Write-Verbose -Message 'Connecting using EXO v2 module'
-        Import-Module ExchangeOnlineManagement -Verbose:$false
-        Connect-ExchangeOnline -Credential $Credential -ShowBanner:$false
+        Write-Verbose -Message 'Connecting using EXO v2 module using MFA'
+        $ConnectionParams = @{
+            ShowBanner  = $false
+            CommandName = $CmdletList
+        }
+
+        if ($Credential) {
+                $ConnectionParams.Credential = $Credential
+        } elseif ($UserPrincipalName) {
+                $ConnectionParams.UserPrincipalName = $UserPrincipalName
+        }
+        if ($DelegatedOrganization) { $ConnectionParams.DelegatedOrganization = $DelegatedOrganization }
+        #Import-Module ExchangeOnlineManagement -Verbose:$false
+        Connect-ExchangeOnline @ConnectionParams
     } else {
         Write-Warning -Message 'EXO v2 module not available, importing direct PS session'
+        if (-not $Credential) {
+            $Credential = Get-Credential -Message 'Enter credential for Office365 tenant'
+        }
         $SessionProps = @{
             ConfigurationName = 'Microsoft.Exchange'
             ConnectionUri     = 'https://outlook.office365.com/powershell-liveid/'
@@ -106,7 +140,7 @@ try {
             Authentication    = 'Basic'
         }
         $Session = New-PSSession @SessionProps -AllowRedirection -Verbose:$false
-        $null = Import-PSSession -Session $Session -DisableNameChecking -CommandName Get-DistributionGroup, Get-DistributionGroupMember -Verbose:$false
+        $null = Import-PSSession -Session $Session -DisableNameChecking -CommandName $CmdletList -Verbose:$false
     }
 }
 
