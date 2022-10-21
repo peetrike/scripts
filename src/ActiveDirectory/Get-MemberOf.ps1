c29f1ffce3abd44a4cdd8e037cf48cc5fb84c8b0
Peter Wawa <meelisn@outlook.com>
2022-10-21 14:12:19 +0300

    add script to find principal's memberof groups

---
+++ src/ActiveDirectory/Get-MemberOf.ps1
@@ -0,0 +1,109 @@
+﻿#Requires -Version 3
+#Requires -Modules ActiveDirectory
+
+<#PSScriptInfo
+    .VERSION 0.0.1
+    .GUID 2763ebae-f04c-4fa2-8ede-fbbdb4ecdadd
+
+    .AUTHOR CPG4285
+    .COMPANYNAME Telia Eesti
+    .COPYRIGHT (c) Telia Eesti 2022.  All rights reserved.
+
+    .TAGS ActiveDirectory, AD, group, memberof
+
+    .LICENSEURI https://opensource.org/licenses/MIT
+    .PROJECTURI https://github.com/peetrike/scripts
+    .ICONURI
+
+    .EXTERNALMODULEDEPENDENCIES ActiveDirectory
+    .REQUIREDSCRIPTS
+    .EXTERNALSCRIPTDEPENDENCIES
+
+    .RELEASENOTES
+        [0.0.1] - 2022-10-21 - Initial release
+
+    .PRIVATEDATA
+#>
+
+<#
+    .SYNOPSIS
+        Short description
+
+    .DESCRIPTION
+        Find groups the account is member of
+
+    .EXAMPLE
+        Get-MemberOf.ps1 -Param1 'Done'
+        Explanation of what the example does
+    .INPUTS
+        Inputs (if any)
+    .OUTPUTS
+        List of groups
+    .NOTES
+        General notes
+    .LINK
+        https://learn.microsoft.com/powershell/module/activedirectory/get-adprincipalgroupmembership
+#>
+
+[CmdletBinding(
+    DefaultParameterSetName = 'Parameter Set 1'
+)]
+#[Alias('')]
+[OutputType([Microsoft.ActiveDirectory.Management.ADGroup])]
+
+param (
+        [Parameter(
+            Mandatory = $true,
+            ValueFromPipeline = $true
+        )]
+        [Alias("p1")]
+        [Microsoft.ActiveDirectory.Management.ADObject]
+        # Param1 help description
+    $AdPrincipal,
+
+        [Parameter(ParameterSetName = 'Parameter Set 1')]
+        [int]
+        # Param2 help description
+    $Level = 1,
+        [bool]
+    $Unique = $true
+)
+
+begin {
+    function GroupMembership {
+        [CmdletBinding()]
+        param (
+                [Parameter(
+                    Mandatory = $true,
+                    ValueFromPipeline = $true
+                )]
+                [Microsoft.ActiveDirectory.Management.ADObject]
+            $AdPrincipal,
+
+                [Parameter(ParameterSetName = 'Parameter Set 1')]
+                [int]
+                # Param2 help description
+            $Level = 1
+        )
+        process {
+            $AdPrincipal
+
+            if ($level -ne 0) {
+                Write-Verbose -Message ('Processing principal: {0}' -f $AdPrincipal.Name)
+                Get-ADPrincipalGroupMembership $AdPrincipal |
+                    GroupMembership -level ($Level - 1)
+            }
+        }
+    }
+}
+
+process {
+    $result = $AdPrincipal | GroupMembership -Level $Level | Select-Object -Skip 1
+    if ($Level -eq 1) { $Unique = $false }
+
+    if ($Unique) {
+        $result | Sort-Object -Unique
+    } else {
+        $result
+    }
+}
---
+++ src/Azure/New-AzureCertificate.ps1
@@ -0,0 +1,77 @@
+﻿#Requires -Modules PKI
+
+<#
+    .SYNOPSIS
+        Generate certificate for application-based authentication in Azure
+    .LINK
+        https://docs.microsoft.com/azure/active-directory/develop/howto-create-self-signed-certificate
+    .LINK
+        https://docs.microsoft.com/powershell/module/pki/new-selfsignedcertificate
+#>
+
+[CmdletBinding()]
+param (
+        [string]
+    $FriendlyName = 'Azure App Authentication',
+        [datetime]
+    $ExpirationDate = [datetime]::Today.AddYears(2),
+        [ValidateSet(
+            'Computer',
+            'User'
+        )]
+        [string]
+    $Target = 'User',
+        [Alias('CertPath', 'Path')]
+        [string]
+    $CertificatePath = $PWD,
+        [switch]
+    $Export
+)
+
+if ([Environment]::OSVersion.Version -notlike '10.*') {
+    throw 'This script requires Windows 10/Server 2016 to function'
+}
+
+$ComputerFqdn = ([Net.Dns]::GetHostEntry('')).HostName
+
+switch ($Target) {
+    'User' {
+        $CertStore = 'Cert:\CurrentUser\My'
+        $SubjectName = '{0}@{1}' -f $env:USERNAME, $ComputerFqdn
+    }
+    'Computer' {
+        $CertStore = 'Cert:\LocalMachine\My'
+        $SubjectName = $ComputerFqdn
+    }
+}
+
+$KeyExportPolicy = if ($Export.IsPresent) {
+    'Exportable'
+} else { 'NonExportable' }
+
+$NewCertProps = @{
+    FriendlyName      = $FriendlyName
+    Subject           = $SubjectName
+    CertStoreLocation = $CertStore
+    NotAfter          = $ExpirationDate
+    KeyExportPolicy   = $KeyExportPolicy
+    KeySpec           = 'Signature' #[Microsoft.CertificateServices.Commands.KeySpec]::Signature
+    KeyLength         = 2048
+    KeyAlgorithm      = 'RSA'       # Azure AD currently supports only RSA
+    HashAlgorithm     = 'SHA256'
+}
+
+$FilePath = Join-Path -Path $CertificatePath -ChildPath ($FriendlyName + '.cer')
+
+$NewCertificate = New-SelfSignedCertificate @NewCertProps
+
+$NewCertificate | Export-Certificate -Type CERT -FilePath $FilePath
+
+if ($Export.IsPresent) {
+    $FilePath = Join-Path -Path $CertificatePath -ChildPath ($FriendlyName + '.pfx')
+    $Password = Get-RandomString
+    $SecurePassword = ConvertTo-SecureString -AsPlainText -Force -String $Password
+    $NewCertificate | Export-PfxCertificate -FilePath $FilePath -Password $SecurePassword
+    Write-Warning -Message ('Write it down: {0}' -f $Password)
+    $NewCertificate | Remove-Item -DeleteKey
+}
