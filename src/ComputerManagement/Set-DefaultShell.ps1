4359de9095681656ebca0154865500f8fd18ead5
Peter Wawa <meelisn@outlook.com>
2023-02-15 17:54:55 +0200

    Add more shells to set

--- src/ComputerManagement/Set-DefaultShell.ps1
+++ src/ComputerManagement/Set-DefaultShell.ps1
@@ -1,4 +1,21 @@
-﻿$WinLogonKey = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
+﻿[CmdletBinding(
+    SupportsShouldProcess
+)]
+param (
+        [ValidateSet(
+            'cmd',
+            'explorer',
+            'powershell'
+        )]
+    $Shell = 'powershell'
+)
 
+$WinLogonKey = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
+
+$CommandLine = @{
+    'cmd'        = 'cmd.exe /k'
+    'explorer'   = 'explorer.exe'
+    'powershell' = 'PowerShell.exe -NoExit'
+}
     # Set PowerShell as default shell
-Set-ItemProperty -Path $WinLogonKey -Name Shell -Value 'PowerShell.exe -NoExit'
+Set-ItemProperty -Path $WinLogonKey -Name Shell -Value $CommandLine.$Shell
