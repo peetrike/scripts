c3d5a428f9069d3ff0b34e999d382191e049e82e
Peter Wawa <meelisn@outlook.com>
2023-05-13 12:36:17 +0300

    Update functions to determine scope path

--- src/ComputerManagement/PowerShell/Clean-ModuleVersion.ps1
+++ src/ComputerManagement/PowerShell/Clean-ModuleVersion.ps1
@@ -54,24 +54,56 @@ param (
     $VersionCount = 2
 )
 
-$PathName = $Scope + 'Modules'
+function Clean-ModuleVersion {
+    [CmdletBinding(
+        SupportsShouldProcess
+    )]
+    param (
+            [Parameter(
+                Position = 1
+            )]
+            [string]
+            [SupportsWildcards()]
+            # Specifies module name to search
+        $Name = '*',
+            [ValidateSet('AllUsers', 'CurrentUser')]
+            [string]
+        $Scope = 'AllUsers',
+            [ValidateRange(1, 10)]
+            [int]
+            # Number of versions to keep
+        $VersionCount = 2
+    )
 
-$ModulePath = $PSGetPath.$PathName
-$SearchPath = Join-Path -Path $ModulePath -ChildPath $Name
+    $PSVersionName = 'PowerShell'
+    if ($PSVersionTable.PSVersion.Major -lt 6) {
+        $PSVersionName = 'Windows' + $PSVersionName
+    }
+    $SearchPattern = switch ($Scope) {
+        'AllUsers' { '{0}\' -f $env:ProgramFiles }
+        'CurrentUser' { '{0}*' -f $env:USERPROFILE }
+    }
+    $ModulePath = $env:PSModulePath -split [IO.Path]::PathSeparator | Where-Object {
+        $_ -like ('{0}{1}\Modules' -f $SearchPattern, $PSVersionName)
+    }
+    $SearchPath = Join-Path -Path $ModulePath -ChildPath $Name
 
-Get-ChildItem -Path $SearchPath -Include * -Directory |
-    Group-Object { $_.Parent.Name } |
-    Where-Object Count -GT $VersionCount |
-    ForEach-Object {
-        $ModuleName = $_.Name
-        Write-Verbose -Message ('Processing module: {0}' -f $ModuleName)
-        $DeleteList = $_.Group |
-            Sort-Object { [version] $_.Name } -Descending |
-            Select-Object -Skip $VersionCount
-        foreach ($folder in $DeleteList) {
-            $Message = 'Remove module "{0}" version' -f $ModuleName
-            if ($PSCmdlet.ShouldProcess($folder.Name, $Message)) {
-                Remove-Item $folder -Recurse -Force -Confirm:$false
+    Get-ChildItem -Path $SearchPath -Include * -Directory |
+        Group-Object { $_.Parent.Name } |
+        Where-Object Count -GT $VersionCount |
+        ForEach-Object {
+            $ModuleName = $_.Name
+            Write-Verbose -Message ('Processing module: {0}' -f $ModuleName)
+            $DeleteList = $_.Group |
+                Sort-Object { [version] $_.Name } -Descending |
+                Select-Object -Skip $VersionCount
+            foreach ($folder in $DeleteList) {
+                $Message = 'Remove module "{0}" version' -f $ModuleName
+                if ($PSCmdlet.ShouldProcess($folder.Name, $Message)) {
+                    Remove-Item $folder -Recurse -Force -Confirm:$false
+                }
             }
         }
-    }
+}
+
+Clean-ModuleVersion @PSBoundParameters
--- src/ComputerManagement/PowerShell/Find-ModuleVersion.ps1
+++ src/ComputerManagement/PowerShell/Find-ModuleVersion.ps1
@@ -25,11 +25,41 @@ param (
     $VersionCount = 2
 )
 
-$PathName = $Scope + 'Modules'
+function Find-ModuleVersion {
+    [CmdletBinding()]
+    param (
+            [Parameter(
+                Position = 1
+            )]
+            [string]
+            [SupportsWildcards()]
+            # Specifies module name to search
+        $Name = '*',
+            [ValidateSet('AllUsers', 'CurrentUser')]
+            [string]
+        $Scope = 'AllUsers',
+            [ValidateRange(1, 10)]
+            [int]
+        $VersionCount = 2
+    )
 
-$ModulePath = $PSGetPath.$PathName
-$SearchPath = Join-Path -Path $ModulePath -ChildPath $Name
+    $PSVersionName = 'PowerShell'
+    if ($PSVersionTable.PSVersion.Major -lt 6) {
+        $PSVersionName = 'Windows' + $PSVersionName
+    }
+    $SearchPattern = switch ($Scope) {
+        'AllUsers' { '{0}\' -f $env:ProgramFiles }
+        'CurrentUser' { '{0}*' -f $env:USERPROFILE }
+    }
+    $ModulePath = $env:PSModulePath -split [IO.Path]::PathSeparator | Where-Object {
+        $_ -like ('{0}{1}\Modules' -f $SearchPattern, $PSVersionName)
+    }
 
-Get-ChildItem -Path $SearchPath -Include * -Directory |
-    Group-Object { $_.Parent.Name } -NoElement |
-    Where-Object Count -GT $VersionCount
+    $SearchPath = Join-Path -Path $ModulePath -ChildPath $Name
+
+    Get-ChildItem -Path $SearchPath -Include * -Directory |
+        Group-Object { $_.Parent.Name } -NoElement |
+        Where-Object Count -GT $VersionCount
+}
+
+Find-ModuleVersion @PSBoundParameters
