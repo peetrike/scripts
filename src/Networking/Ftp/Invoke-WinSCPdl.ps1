9ab50db6d5ea45194212bbbea6eb247a3dc8cc5b
Peter Wawa <meelisn@outlook.com>
2023-06-01 17:11:57 +0300

    Refactor script
    - add example how to provide connection information from external file
    - Cleanup Write-Log
    - extract file size calculation

--- src/Networking/Ftp/Invoke-WinSCPdl.ps1
+++ src/Networking/Ftp/Invoke-WinSCPdl.ps1
@@ -1,15 +1,17 @@
+#Requires -Version 2
+
 <#
     .SYNOPSIS
         Sample script that downloads files using WinSCP
     .DESCRIPTION
         This script downloads files using WinSCP .NET component
     .NOTES
-        Information or caveats about the function e.g. 'This function is not supported in Linux'
+        You need installed WinSCP or Assembly .DLL from https://winscp.net/eng/docs/library_install
     .LINK
         https://winscp.net/eng/docs/library_powershell#example
     .EXAMPLE
-        Test-MyTestFunction -Verbose
-        Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+        Get-Content .\connection.json | ConvertFrom-Json | .\Invoke-WinSCPdl.ps1
+        Take connection details from .json file and pass them to script
 #>
 
 [CmdletBinding(SupportsShouldProcess)]
@@ -26,6 +28,7 @@ param (
         [Parameter(ValueFromPipelineByPropertyName = $true)]
         [string]
     $SshHostKeyFingerprint,
+
     $LocalPath = $PWD,
     $RemotePath = '/',
     $Pattern = '*',
@@ -41,26 +44,22 @@ begin {
             $Message,
                 [Parameter(Position = 1)]
                 [string]
-            $Log = ".\Log.txt"
+            $Log = $LogFilePath
         )
 
         process {
-            Write-Verbose $Message
-
-            if ($WriteLog -or $Message) {
-                if ($LogFilePath) {
-                    $LogFile = $LogFilePath
-                } else {
-                    $LogFile = $Log
-                }
-
-                "$(Get-Date) $Message" | Out-File -FilePath $LogFile -Append
+            if ($Message) {
+                Write-Verbose $Message
+                $LogMessage = '{0} - {1}' -f [datetime]::Now, $Message
+                $LogMessage | Out-File -FilePath $Log -Append
             }
         }
     }
+
 }
 
 process {
+        #determine, where the Assembly is installed
     $RegPath = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\winscp*'
     $assemblyPath = if ($env:WINSCP_PATH) {
         $env:WINSCP_PATH
@@ -83,24 +82,25 @@ process {
         $session.Open($sessionOptions)
 
         $directoryInfo = $session.ListDirectory($remotePath)
-        $File = $directoryInfo.Files | Where-Object { (-Not $_.IsDirectory) -and $_.Name -like $pattern }
+        $File = $directoryInfo.Files | Where-Object { (-Not $_.IsDirectory) -and $_.Name -like $Pattern }
 
         foreach ($f in $File) {
             ('Downloading file: {0}{1}' -f $remotePath, $f.Name) | Write-Log
 
             $session.GetFiles($session.EscapeFileMask($remotePath + $f.Name), $localPath).Check()
-            $filepath = Join-Path $localPath "$($f.Name)"
+            $filepath = Join-Path $localPath $f.Name
 
             if (Test-Path $filepath) {
                 ('The file is downloaded to {0}' -f $filepath) | Write-Log
 
-                if ((Get-ChildItem $filepath).Length -eq $f.Length) {
-                    ('The downloaded file lenght {0} is the same as in FTP server' -f (Get-ChildItem $filepath).Length) | Write-Log
+                $fileSize = (Get-ChildItem $filepath).Length
+                if ($fileSize -eq $f.Length) {
+                    ('The downloaded file length {0} is the same as in FTP server' -f $fileSize) | Write-Log
                     $session.RemoveFiles($session.EscapeFileMask($remotePath + $f.Name)) | Write-Log
                 }
             }
         }
-    } catch [Exception] {
+    } catch {
         $_.Exception.Message
     } finally {
         $session.Dispose()
