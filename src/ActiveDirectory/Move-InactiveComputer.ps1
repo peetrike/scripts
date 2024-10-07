3708ec8f3691a3116348f7614def1a59c47c6264
Peter Wawa <meelisn@outlook.com>
2024-10-07 10:15:39 +0300

    add computer account disable script

---
+++ src/ActiveDirectory/Move-InactiveComputer.ps1
@@ -0,0 +1,30 @@
+﻿[CmdletBinding(
+    SupportsShouldProcess
+)]
+param (
+        [Parameter(
+            Mandatory
+        )]
+        [string]
+    $Destination,
+        [int]
+    $DaysAgo = 90
+)
+
+$startDate = (Get-Date).AddDays(-$DaysAgo)
+$filter = {
+    LastLogonDate -lt $startDate -and
+    OperatingSystem -notlike '*server*' -and
+    Enabled -eq $true
+}
+$propertyList = @(
+    'OperatingSystem'
+    'LastLogonDate'
+)
+
+foreach ($Computer in Get-ADComputer -Filter $filter -Properties $propertyList) {
+    if ($PSCmdlet.ShouldProcess($Computer.Name, 'Disable and move computer')) {
+        Disable-ADAccount -Identity $Computer.DistinguishedName -PassThru |
+            Move-ADObject -TargetPath $Destination -PassThru
+    }
+}
