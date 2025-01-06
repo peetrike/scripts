84fab1ca9ccfad37f26c0d84c4309cf4b974beba
Peter Wawa <meelisn@outlook.com>
2025-01-06 14:20:46 +0200

    Change function
    - use hashtable filter for events
    - add -First parameter to limit number of returned events
    - add ability to return failure & success events
    - rename function (and script)

---
+++ src/ComputerManagement/EventLog/Get-WindowsUpdateEvent.ps1
@@ -0,0 +1,77 @@
+﻿[CmdletBinding()]
+param (
+        [ValidateSet('Failure', 'Success')]
+        [string[]]
+    $Type = 'Failure',
+        [Alias('StartTime')]
+        [datetime]
+        # Specifies start time of events
+    $After,
+        [Alias('EndTime')]
+        [datetime]
+        # Specifies end time of events
+    $Before,
+        [int]
+        # Specifies maximum number of events to return
+    $First
+)
+
+
+function Get-WindowsUpdateEvent {
+    [CmdletBinding()]
+    param (
+            [ValidateSet('Failure', 'Success')]
+            [string[]]
+        $Type = 'Failure',
+            [Alias('StartTime')]
+            [datetime]
+            # Specifies start time of events
+        $After,
+            [Alias('EndTime')]
+            [datetime]
+            # Specifies end time of events
+        $Before,
+            [int]
+            # Specifies maximum number of events to return
+        $First
+    )
+
+    $EventList = @{
+        Failure = 20
+        Success = 19
+    }
+
+    $EventFilter = @{
+        ProviderName = 'Microsoft-Windows-WindowsUpdateClient'
+        Id           = foreach ($currentType in $Type) { $EventList.$currentType }
+    }
+
+    if ($After) {
+        $EventFilter.StartTime = $After
+    }
+    if ($Before) {
+        $EventFilter.EndTime = $Before
+    }
+
+    $EventProps = @{}
+    if ($First) {
+        $EventProps.MaxEvents = $First
+    }
+
+    Write-Debug -Message ("Using filter:`n{0}" -f ($EventFilter | Out-String))
+
+    foreach ($currentEvent in Get-WinEvent -FilterHashtable $EventFilter @EventProps) {
+        $TimeCreated = $currentEvent.TimeCreated
+        $xmlEvent = [xml] $currentEvent.ToXml()
+        $UpdateTitle = $xmlEvent.SelectSingleNode('//*[@Name = "updateTitle"]').InnerText
+
+        [PSCustomObject] @{
+            TimeCreated  = $TimeCreated
+            ComputerName = $currentEvent.MachineName
+            ErrorCode    = $xmlEvent.SelectSingleNode('//*[@Name = "errorCode"]').InnerText
+            UpdateTitle  = $UpdateTitle
+        }
+    }
+}
+
+Get-WindowsUpdateEvent @PSBoundParameters
