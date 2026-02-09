d50e6b5d057e97ce92704290de8889a347523d43
Peter Wawa <meelisn@outlook.com>
2026-02-09 16:16:52 +0200

    refactor script
    - make friendlyname static
    - generate filename from fist ComputerFqdn value

--- src/ComputerManagement/WinRM/New-ServerCertificate.ps1
+++ src/ComputerManagement/WinRM/New-ServerCertificate.ps1
@@ -3,7 +3,7 @@
 
 <#
     .SYNOPSIS
-        Obtains certificate used by WinRM over HTTPS
+        Creates certificate for WinRM over HTTPS
     .LINK
         https://learn.microsoft.com/troubleshoot/windows-client/system-management-components/configure-winrm-for-https
     .LINK
@@ -31,35 +31,35 @@ param (
     $Export
 )
 
-if ([Environment]::OSVersion.Version -notlike '10.*') {
-    throw 'This script requires Windows 10/Server 2016 to function'
-}
+end {
+    $KeyExportPolicy = if ($Export.IsPresent) {
+        'Exportable'
+    } else { 'NonExportable' }
 
-$KeyExportPolicy = if ($Export.IsPresent) {
-    'Exportable'
-} else { 'NonExportable' }
+    $NewCertProps = @{
+        FriendlyName      = 'WinRM self-signed'
+        DnsName           = $ComputerFqdn
+        CertStoreLocation = 'Cert:\LocalMachine\My'
+        KeyExportPolicy   = $KeyExportPolicy
+        KeySpec           = 'Signature' #[Microsoft.CertificateServices.Commands.KeySpec]::Signature
+        TextExtension     = '2.5.29.37={text}1.3.6.1.5.5.7.3.1'     # server authentication
+        KeyLength         = 4096
+        HashAlgorithm     = 'SHA256'
+    }
 
-$NewCertProps = @{
-    FriendlyName      = '{0} self-signed' -f $ComputerFqdn[0]
-    DnsName           = $ComputerFqdn
-    CertStoreLocation = 'Cert:\LocalMachine\My'
-    KeyExportPolicy   = $KeyExportPolicy
-    KeySpec           = 'Signature' #[Microsoft.CertificateServices.Commands.KeySpec]::Signature
-    TextExtension     = '2.5.29.37={text}1.3.6.1.5.5.7.3.1'     # server authentication
-    KeyLength         = 4096
-    HashAlgorithm     = 'SHA256'
-}
+    $NewCertificate = New-SelfSignedCertificate @NewCertProps
 
-$NewCertificate = New-SelfSignedCertificate @NewCertProps
+    $FilePath = Join-Path -Path $CertificatePath -ChildPath ('{0} self-signed.cer' -f $ComputerFqdn[0])
+    $NewCertificate | Export-Certificate -Type CERT -FilePath $FilePath
 
-$FilePath = Join-Path -Path $CertificatePath -ChildPath ($NewCertProps.FriendlyName + '.cer')
-$NewCertificate | Export-Certificate -Type CERT -FilePath $FilePath
-
-if ($Export.IsPresent) {
-    $FilePath = Join-Path -Path $CertificatePath -ChildPath ($FriendlyName + '.pfx')
-    $Password = Get-RandomString
-    $SecurePassword = ConvertTo-SecureString -AsPlainText -Force -String $Password
-    $NewCertificate | Export-PfxCertificate -FilePath $FilePath -Password $SecurePassword
-    Write-Warning -Message ('Write it down: {0}' -f $Password)
-    $NewCertificate | Remove-Item -DeleteKey
+    if ($Export) {
+        $FilePath = Join-Path -Path $CertificatePath -ChildPath ('{0} self-signed.pfx' -f $ComputerFqdn[0])
+        $Password = Get-RandomString -Length 15
+        $SecurePassword = ConvertTo-SecureString -AsPlainText -Force -String $Password
+        $NewCertificate | Export-PfxCertificate -FilePath $FilePath -Password $SecurePassword
+        $NewCertificate | Remove-Item -DeleteKey
+        Get-Item -Path $FilePath | Add-Member -MemberType NoteProperty -Name Password -Value $Password -PassThru
+        Write-Warning -Message ('Write it down: {0}' -f $Password)
+        Import-PfxCertificate -FilePath $FilePath -Password $SecurePassword
+    }
 }
