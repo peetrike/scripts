4f9698087f3ab4390b0032ef8f977af5ab207ae0
Peter Wawa <meelisn@outlook.com>
2026-02-09 12:17:28 +0200

    move scripts

---
+++ src/ComputerManagement/Set-HttpsRemoting.ps1
@@ -0,0 +1,54 @@
+﻿<#
+    .SYNOPSIS
+        Configure HTTPS remoting
+    .DESCRIPTION
+        Configure WinRM to use HTTPS remoting
+    .EXAMPLE
+        .\set-HttpsRemoting.ps1 -ComputerFqdn 'myserver.domain.com'
+
+        This example configures HTTPS remoting using specified FQDN
+#>
+[CmdletBinding()]
+param (
+        [string]
+        # Specifies certificate FQDN to use for HTTPS remoting
+    $ComputerFqdn = ([Net.Dns]::GetHostEntry('')).HostName
+)
+
+$ThumbPrint = @(
+    Get-ChildItem -Path Cert:\LocalMachine\my |
+        Where-Object DnsNameList -like $ComputerFqdn |
+        Sort-Object NotAfter -Descending
+)[0].Thumbprint
+
+$ValueSet = @{
+    CertificateThumbprint = $ThumbPrint
+}
+$SelectorSplat = @{
+    ResourceURI = 'winrm/config/listener'
+    SelectorSet = @{ Address = '*'; Transport = 'https' }
+}
+if (Get-WSManInstance @SelectorSplat -ErrorAction SilentlyContinue) {
+    Write-Verbose -Message 'Replacing Cert thumbprint in existing listener'
+    Set-WSManInstance @SelectorSplat -ValueSet $ValueSet
+} else {
+    Write-Verbose -Message 'Creating new listener'
+    $ValueSet.Hostname = $ComputerFqdn
+    New-WSManInstance @SelectorSplat -ValueSet $ValueSet
+}
+
+$RuleSplat = @{
+    Name        = 'WINRM-HTTPS-In-TCP'
+    DisplayName = 'Windows Remote Management (HTTPS-In)'
+    Group       = '@FirewallAPI.dll,-30267'
+    Direction   = 'Inbound'
+    Protocol    = 'TCP'
+    LocalPort   = 5986
+    Action      = 'Allow'
+}
+try {
+    Get-NetFirewallRule -Name $RuleSplat.Name -ErrorAction Stop | Set-NetFirewallRule -Enabled True
+} catch {
+    Write-Verbose -Message 'Creating new firewall rule'
+    New-NetFirewallRule @RuleSplat
+}
---
+++ src/RdpServer/Replace-RdpCertificate.ps1
@@ -0,0 +1,31 @@
+﻿<#
+.SYNOPSIS
+    Replace the RDP certificate.
+.DESCRIPTION
+    This script replaces the RDP certificate with the one that uses the specified FQDN.
+#>
+
+[CmdletBinding()]
+param (
+        [string]
+    $ComputerFqdn = ([Net.Dns]::GetHostEntry('')).HostName
+)
+
+$ThumbPrint = @(
+    Get-ChildItem -Path Cert:\LocalMachine\my |
+        Where-Object DnsNameList -like $ComputerFqdn |
+        Sort-Object NotAfter -Descending
+)[0].Thumbprint
+
+$getCimInstanceSplat = @{
+    ClassName = 'Win32_TSGeneralSetting'
+    Namespace = 'root\CIMv2\TerminalServices'
+    Filter    = 'TerminalName = "RDP-Tcp"'
+}
+
+Get-CimInstance @getCimInstanceSplat |
+    Set-CimInstance -Property @{
+        SSLCertificateSHA1Hash = $ThumbPrint
+    }
+
+Get-Service -Name 'TermService' | Restart-Service -Force
