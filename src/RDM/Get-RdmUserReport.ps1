d7c81c28bfc9925f9b5bcbaad2d0622b121d18e8
Peter Wawa <meelisn@outlook.com>
2026-02-19 15:48:11 +0200

    add new script

---
+++ src/RDM/Get-RdmUserReport.ps1
@@ -0,0 +1,24 @@
+﻿#Requires -Version 7
+#Requires -Modules Devolutions.PowerShell
+
+[CmdletBinding()]
+param (
+        [ValidateScript({
+            Get-RDMDataSource -Name $_
+        })]
+        [string]
+        # RDM Data Source to be used
+    $DataSource = (Get-RDMCurrentDataSource).Name
+)
+
+if ($DataSource) {
+    Get-RDMDataSource -Name $DataSource | Set-RDMCurrentDataSource
+}
+
+Write-Verbose -Message ('Working with Data Source: {0}' -f $DataSource)
+
+Get-RDMUser |
+    Select-Object -Property Name, Description, Email, HasAccessRdm, IsEnabled, @{
+        Name       = 'Database'
+        Expression = { $DataSource }
+    }
